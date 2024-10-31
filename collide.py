04ca65786fff4d12710dedb977b6b9924d369f01
Mark02496 <markwahayden@gmail.com>
2024-10-31 14:59:40 -0700

    elastic collisions

--- collide.py
+++ collide.py
@@ -53,24 +53,6 @@ while running:
             directions[p], directions[r]= directions[r], directions[p]
 
 
-    '''
-    for n in parts:
-            if directions[n]:  # Moving right
-                n.x += n.v * dt
-                if n.x > 1:
-                    directions[n] = False
-            else:  # Moving left
-                n.x -= n.v * dt
-                if n.x < 0:
-                    directions[n] = True
-
-            # Check for collision and swap velocities
-            if (n == p and 0 < r.x - p.x <= 0.01) or (n == r and 0 < p.x - r.x <= 0.01):
-                p.v, r.v = r.v, p.v  # Swap velocities
-                if directions[p] == directions[r]:
-                    directions[p] = not directions[p]
-                    directions[r] = not directions[r]
-    '''
     p.draw()
     r.draw()
     pygame.display.flip()
